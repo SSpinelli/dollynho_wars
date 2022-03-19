c7a2831b4f93a3f2bd35ea0a4f395c12613ae17c
FernandoMos92 <fernandomosp@gmail.com>
2022-03-18 21:42:08 -0300

    feat: complete exercise kata 7

--- kata6/dollyEnglishTeacher.js
+++ kata6/dollyEnglishTeacher.js
@@ -7,7 +7,8 @@
 */
 
 function isPangram(phrase) {
-  //Sua solução aqui!
+  const alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+  return alphabet.every(el => phrase.toLowerCase().includes(el));
 }
 
 module.exports = isPangram;
--- kata7/dollyschamps.js
+++ kata7/dollyschamps.js
@@ -13,8 +13,21 @@
     [2015, 8, 26] ===> "3479"
 */
 
-function leftovers(array) {
-  //Sua solução vai aqui!
+const array3 = [];
+
+const leftovers = (array) => {
+  const arrayNumber = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+  const returnArr = arrayNumber.filter(el => !array.join().split('').includes(el))
+  let msgString = '';
+  if(returnArr.join('') === '0123456789') {
+    msgString = 'Sobrou todos os números'; 
+  } else if (returnArr.join('') === '') {
+    msgString = 'Não sobrou nenhum número'; 
+  } else {
+    return returnArr.join('')
+  }
+  return msgString;
 }
+console.log(leftovers(array3));
 
 module.exports = leftovers;
